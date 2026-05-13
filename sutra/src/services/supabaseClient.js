081e329a3689d8a63610f2734c7d82793ac0e790
Anushka <lavianu2004@gmail.com>
2026-05-13 20:07:15 +0530

    env variables updated

--- sutra/src/services/supabaseClient.js
+++ sutra/src/services/supabaseClient.js
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://grjwlgdllrmqeasugeqp.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdyandsZ2RsbHJtcWVhc3VnZXFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxMzA5MjgsImV4cCI6MjA5MzcwNjkyOH0.VDg0wtv0qIgez8XOBnJAFtB-3PxFJot2prxFYNCuMuE'
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseKey
+)
