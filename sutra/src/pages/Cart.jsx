08d3afa9f486aac15a5e3ed0bdcea0f193eef6d8
Anushka <lavianu2004@gmail.com>
2026-06-20 17:47:21 +0530

    added cart page

--- sutra/src/pages/Cart.jsx
+++ sutra/src/pages/Cart.jsx
@@ -1,14 +1,19 @@
 import { useContext } from 'react'
-
 import { CartContext } from '../context/CartContext'
+import './Cart.css'
 
 function Cart() {
 
   const { cart } = useContext(CartContext)
 
+  const total = cart.reduce(
+    (sum, item) => sum + Number(item.price),
+    0
+  )
+
   return (
 
-    <div style={{padding:"40px"}}>
+    <div className="cart-container">
 
       <h1>My Cart</h1>
 
@@ -20,17 +25,48 @@ function Cart() {
 
         :
 
-        cart.map((item)=>(
+        <>
 
-          <div key={item.id}>
+          {
 
-            <h3>{item.name}</h3>
+            cart.map((item)=>(
 
-            <p>₹{item.price}</p>
+              <div className="cart-card" key={item.id}>
+
+                <img
+                  src={item.image}
+                  alt={item.name}
+                />
+
+                <div className="cart-info">
+
+                  <h2>{item.name}</h2>
+
+                  <p>{item.description}</p>
+
+                  <h3>₹{item.price}</h3>
+
+                </div>
+
+              </div>
+
+            ))
+
+          }
+
+          <div className="summary">
+
+            <h2>Total : ₹{total}</h2>
+
+            <button>
+
+              Proceed To Checkout
+
+            </button>
 
           </div>
 
-        ))
+        </>
 
       }
 
