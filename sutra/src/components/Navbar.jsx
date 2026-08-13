5698c41c07e63663c54758897c7740776c9bafb3
Anushka <lavianu2004@gmail.com>
2026-08-13 22:58:22 +0530

    Added logout feature

--- sutra/src/components/Navbar.jsx
+++ sutra/src/components/Navbar.jsx
@@ -1,67 +1,59 @@
 import './Navbar.css'
-import { Link, useNavigate } from 'react-router-dom'
-import { useContext, useState } from 'react'
-import { FiHeart, FiShoppingBag } from 'react-icons/fi'
+import { Link,useNavigate } from 'react-router-dom'
+import { useContext,useState } from 'react'
+import { FiHeart,FiShoppingBag } from 'react-icons/fi'
 import { WishlistContext } from '../context/WishlistContext'
 import { CartContext } from '../context/CartContext'
 import { SearchContext } from '../context/SearchContext'
+import { AuthContext } from '../context/AuthContext'
 import logo from '../assets/images/sutra-logo.png'
 
-function Navbar() {
+function Navbar(){
+  const navigate=useNavigate()
+  const { wishlist }=useContext(WishlistContext)
+  const { cart }=useContext(CartContext)
+  const { setSearch }=useContext(SearchContext)
+  const { user,logout }=useContext(AuthContext)
+  const [input,setInput]=useState('')
 
-  const navigate = useNavigate()
-
-  const { wishlist } = useContext(WishlistContext)
-  const { cart } = useContext(CartContext)
-  const { setSearch } = useContext(SearchContext)
-
-  const [input, setInput] = useState('')
-
-  const cartCount = cart.reduce(
-    (sum, item) => sum + item.quantity,
+  const cartCount=cart.reduce(
+    (sum,item)=>sum+item.quantity,
     0
   )
 
   function handleSearch(e){
-
     if(e.key==="Enter"){
-
       setSearch(input)
-
       navigate("/",{
         state:{
           scrollToProducts:true
         }
       })
-
     }
-
   }
 
   function handleLogoClick(){
-
     setSearch("")
     setInput("")
-
     navigate("/")
+  }
 
+  async function handleLogout(){
+    await logout()
+    navigate("/")
   }
 
   return(
-
     <nav className="navbar">
-
       <Link
         to="/"
         className="logo"
         onClick={handleLogoClick}
       >
-
         <img
           src={logo}
           alt="Sutrā"
         />
-
       </Link>
 
       <input
@@ -74,49 +66,54 @@ function Navbar() {
       />
 
       <div className="nav-icons">
-
         <Link
           to="/wishlist"
           className="icon-btn"
         >
-
           <FiHeart />
-
           {
             wishlist.length>0 &&
             <span className="badge">
               {wishlist.length}
             </span>
           }
-
         </Link>
 
         <Link
           to="/cart"
           className="icon-btn"
         >
-
           <FiShoppingBag />
-
           {
             cartCount>0 &&
             <span className="badge">
               {cartCount}
             </span>
           }
-
         </Link>
 
-        <button className="login-btn">
-          Login
-        </button>
-
+        {
+          user ? (
+            <div className="account-menu">
+              <button
+                className="login-btn"
+                onClick={handleLogout}
+              >
+                Logout
+              </button>
+            </div>
+          ) : (
+            <button
+              className="login-btn"
+              onClick={()=>navigate('/login')}
+            >
+              Login
+            </button>
+          )
+        }
       </div>
-
     </nav>
-
   )
-
 }
 
 export default Navbar
